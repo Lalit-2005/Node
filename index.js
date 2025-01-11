3bbad23e5aa31169fd402c51c0a8f8c56999df4f
Lalit <145419475+Lalit-2005@users.noreply.github.com>
2025-01-11 18:59:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const http =require("http")
 const fs=require("fs")
-const PORT=process.env.PORT;
+const PORT=process.env.PORT || 4000
 const home=fs.readFileSync("./index.html","utf-8")
 const server=http.createServer((req,res)=>{
     if(req.url==="/")
