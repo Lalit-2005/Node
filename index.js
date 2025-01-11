d81a83e3744291e0bcb400648819cf9bdd0d7d68
LalitChauhan <lalitchauhan2005242@gmail.com>
2025-01-11 18:11:20 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const http =require("http")
+const fs=require("fs")
+const PORT=4000
+const hostname="localhost"
+const home=fs.readFileSync("./index.html","utf-8")
+const server=http.createServer((req,res)=>{
+    if(req.url==="/")
+    {
+        return res.end(home);
+    }
+    if(req.url==="/about")
+    {
+        return res.end("<h1>hello my home page</h1>");
+    }
+    if(req.url==="/contact")
+    {
+        return res.end("<h1>hello my contact page</h1>");
+    }
+    else{
+        return res.end("<h1>404 this is error</h1>")
+    }
+})
+server.listen(PORT,hostname,()=>{
+    console.log(`server is running on http://${hostname}:${PORT}`)
+})
