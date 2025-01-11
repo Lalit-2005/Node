1ec0064a2ca69a46d719cda54feed85250924c42
Lalit <145419475+Lalit-2005@users.noreply.github.com>
2025-01-11 19:58:18 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,24 +1,17 @@
-const http = require("http");
 const fs = require("fs");
-const PORT = process.env.PORT || 3000;
-const home=fs.readFileSync("./index.html","utf-8")
-const server=http.createServer((req,res)=>{
-    if(req.url==="/")
-    {
-        return res.end(home);
+const path = require("path");
+
+module.exports = (req, res) => {
+    const home = fs.readFileSync(path.join(__dirname, "index.html"), "utf-8");
+
+    if (req.url === "/") {
+        res.setHeader("Content-Type", "text/html");
+        res.end(home);
+    } else if (req.url === "/about") {
+        res.end("<h1>hello my about page</h1>");
+    } else if (req.url === "/contact") {
+        res.end("<h1>hello my contact page</h1>");
+    } else {
+        res.end("<h1>404 this is error</h1>");
     }
-    if(req.url==="/about")
-    {
-        return res.end("<h1>hello my home page</h1>");
-    }
-    if(req.url==="/contact")
-    {
-        return res.end("<h1>hello my contact page</h1>");
-    }
-    else{
-        return res.end("<h1>404 this is error</h1>")
-    }
-})
-server.listen(PORT,()=>{
-    console.log(`server is running on ${PORT}`)
-})
+};
