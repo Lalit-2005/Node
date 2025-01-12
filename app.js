19b2a48632944f4419a5c7428ced76fc7cab9f0b
Lalit <145419475+Lalit-2005@users.noreply.github.com>
2025-01-12 10:43:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,29 +3,24 @@ const http = require("http");
 const fs = require("fs");
 const path = require("path");
 
-const PORT = process.env.PORT || 5000;
-
-const home = fs.readFileSync(path.join(_dirname,"index.html"), "utf-8");
+const PORT = process.env.PORT || 4000;
+const home = fs.readFileSync(path.join(__dirname, "index.html"), "utf-8");
 
 
 const server = http.createServer((req, res) => {
-    if (req.url === "/") 
-    {
+    if (req.url === "/") {
         return res.end(home);
     }
-    if (req.url === "/about") 
-    {
+    if (req.url === "/about") {
         return res.end("<h1>hello my home page</h1>");
     }
-    if (req.url === "/contact") 
-    {
+    if (req.url === "/contact") {
         return res.end("<h1>hello my contact page</h1>");
-    } else 
-    {
+    } else {
         return res.end("<h1>404 this is error</h1>");
     }
 });
 
 server.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port "${PORT}`);
 });
